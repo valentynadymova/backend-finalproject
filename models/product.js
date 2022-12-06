7b22ab0c04f93df445a13e8c19786a71712b3a5f
valentynadymova <valentynadymova@gmail.com>
2022-12-06 20:26:50 +0100

    priduct model

--- models/product.js
+++ models/product.js
@@ -1,63 +1,49 @@
-// const Joi=require('joi');
-// const { Schema, model } = require('mongoose');
+const Joi=require('joi');
+const { Schema, model } = require('mongoose');
 
-// const productSchema = Schema(
-//   {
-//     categories: { type: Array },
-//     weight: { type: Number },
-//     title: {
-//       type: {
-//         ua: { type: String },
-//         ru: {type: String}
-//       },
-//     },
-//     calories: { type: Number },
-//     groupBloodNotAllowed: {
-//       type: Array,
-//     },
+const productSchema = Schema(
+  {
+    categories: { type: Array },
+    weight: { type: Number },
+    title: {
+      type: {
+        ua: { type: String },
+        ru: {type: String}
+      },
+    },
+    calories: { type: Number },
+    groupBloodNotAllowed: {
+      type: Array,
+    },
+  },
+  { versionKey: false, timestamps: true },
+);
+
+
+// const productSchema = new Schema({
+//   _id: {
+//     type: Object,
 //   },
-//   { versionKey: false, timestamps: true },
-// );
-
-// const inputData = Joi.object({
-//   height: Joi.number().required(),
-//   age: Joi.number().required(),
-//   currentWeight: Joi.number().required(),
-//   desiredWeight: Joi.number().required(),
-//   bloodType: Joi.number().required(),
+//   categories: {
+//     type: Array,
+//   },
+//   weight: {
+//     type: Number,
+//     default: 100,
+//   },
+//   title: {
+//     type: Object,
+//   },
+//   calories: {
+//     type: Number,
+//   },
+//   groupBloodNotAllowed: {
+//     1: Boolean,
+//     2: Boolean,
+//     3: Boolean,
+//     4: Boolean,
+//   },
 // });
-
-// const Product = model('product', productSchema);
-
-// module.exports = { Product, inputData };
-
-const { Schema, model } = require("mongoose");
-const Joi = require("joi");
-
-const productSchema = new Schema({
-  _id: {
-    type: Object,
-  },
-  categories: {
-    type: Array,
-  },
-  weight: {
-    type: Number,
-    default: 100,
-  },
-  title: {
-    type: Object,
-  },
-  calories: {
-    type: Number,
-  },
-  groupBloodNotAllowed: {
-    1: Boolean,
-    2: Boolean,
-    3: Boolean,
-    4: Boolean,
-  },
-});
 
 const inputData = Joi.object({
   height: Joi.number().required(),
